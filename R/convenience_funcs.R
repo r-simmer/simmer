8e524cd4e487fd1fefb89163813b8cf7312d5c3a
Iñaki Úcar <i.ucar86@gmail.com>
2016-02-20 21:43:50 +0100

    add convenience functions

--- R/convenience_funcs.R
+++ R/convenience_funcs.R
@@ -9,32 +9,17 @@
 #' @export
 #'
 #' @examples
-#' t0 <- create_trajectory("my trajectory") %>%
-#'   ## add an intake activity
-#'   seize("nurse", 1) %>%
-#'   timeout(function() rnorm(1, 15)) %>%
-#'   release("nurse", 1) %>%
-#'   ## add a consultation activity
-#'   seize("doctor", 1) %>%
-#'   timeout(function() rnorm(1, 20)) %>%
-#'   release("doctor", 1) %>%
-#'   ## add a planning activity
-#'   seize("administration", 1) %>%
-#'   timeout(function() rnorm(1, 5)) %>%
-#'   release("administration", 1)
-#'   
-#' env <- simmer("SuperDuperSim") %>%
-#'   add_resource("nurse", 1) %>%
-#'   add_resource("doctor", 2) %>%
-#'   add_resource("administration", 1) %>%
-#'   add_generator("patient", t0, at(0, c(1,10,30), 40, 43))
-at<-function(...){
-  time_vec<-unlist(list(...))
-  time_diffs<-c(time_vec[1], diff(time_vec))
-  i<-0
-  function(){
-    if(i<length(time_vec)){
-      i<<-i+1
+#' t0 <- create_trajectory() %>% timeout(0)
+#' env <- simmer(verbose=T) %>%
+#'   add_generator("dummy", t0, at(0, c(1,10,30), 40, 43)) %>%
+#'   run(100)
+at <- function(...) {
+  time_vec <- unlist(list(...))
+  time_diffs <- c(time_vec[1], diff(time_vec))
+  i <- 0
+  function() {
+    if (i < length(time_diffs)) {
+      i <<- i+1
       return(time_diffs[i])
     } else {
       return(-1)
@@ -42,51 +27,142 @@ at<-function(...){
   }
 }
 
-
-#' Generate arrivals starting at a specified time
+#' Arrivals every specific interval
 #' 
-#' Generator convenience function to generate inter arrivals with a specified initial start time.
+#' Generator convenience function to generate arrivals every specific interval.
+#' When the generator reaches the last interval, it starts again from the first one.
 #'
-#' @param start_time the time at which to launch the initial arrival
-#' @param dist a function modelling the interarrival times (returning a negative value stops the generator).
-#' @param arrive if set to \code{TRUE} (default) the first arrival will be generated at \code{start_time} and will follow \code{dist} from then on. If set to \code{FALSE}, will initiate \code{dist} at \code{start_time} (and the first arrival will most likely start at a time later than \code{start_time}).
+#' @param ... a vector or multiple parameters of intervals between arrivals.
 #'
 #' @return Returns a generator function.
 #' @seealso \link{add_generator}
 #' @export
 #'
 #' @examples
-#' t0 <- create_trajectory("my trajectory") %>%
-#'   ## add an intake activity
-#'   seize("nurse", 1) %>%
-#'   timeout(function() rnorm(1, 15)) %>%
-#'   release("nurse", 1) %>%
-#'   ## add a consultation activity
-#'   seize("doctor", 1) %>%
-#'   timeout(function() rnorm(1, 20)) %>%
-#'   release("doctor", 1) %>%
-#'   ## add a planning activity
-#'   seize("administration", 1) %>%
-#'   timeout(function() rnorm(1, 5)) %>%
-#'   release("administration", 1)
-#'   
-#' env <- simmer("SuperDuperSim") %>%
-#'   add_resource("nurse", 1) %>%
-#'   add_resource("doctor", 2) %>%
-#'   add_resource("administration", 1) %>%
-#'   add_generator("patient", t0, from(5, function() rnorm(1, 10)))
-from<-function(start_time, dist, arrive=TRUE){
-  started<-FALSE
-  function(){
-    if(!started){
-      started<<-TRUE
-      if(arrive){
+#' t0 <- create_trajectory() %>% timeout(0)
+#' env <- simmer(verbose=T) %>%
+#'   add_generator("dummy", t0, every(1, 2, 1)) %>%
+#'   run(10)
+every <- function(...) {
+  time_diffs <- unlist(list(...))
+  i <- 0
+  function() {
+    if (i < length(time_diffs)) {
+      i <<- i+1
+    } else {
+      i <<- 1
+    }
+    return(time_diffs[i])
+  }
+}
+
+#' Generate arrivals starting at a specified time
+#' 
+#' Generator convenience function to generate inter-arrivals with a specified start time.
+#'
+#' @param start_time the time at which to launch the initial arrival.
+#' @param dist a function modelling the interarrival times.
+#' @param arrive if set to \code{TRUE} (default) the first arrival will be 
+#' generated at \code{start_time} and will follow \code{dist} from then on. 
+#' If set to \code{FALSE}, will initiate \code{dist} at \code{start_time} 
+#' (and the first arrival will most likely start at a time later than 
+#' \code{start_time}).
+#' 
+#' @return Returns a generator function.
+#' @seealso \link{add_generator}
+#' @export
+#'
+#' @examples
+#' t0 <- create_trajectory() %>% timeout(0)
+#' env <- simmer(verbose=T) %>%
+#'   add_generator("dummy", t0, from(5, function() runif(1, 1, 2))) %>%
+#'   run(10)
+from <- function(start_time, dist, arrive=TRUE) {
+  started <- FALSE
+  function() {
+    if (!started) {
+      started <<- TRUE
+      if (arrive) {
         return(start_time)
       } else {
         return(start_time + dist())
       }
     } else {
       return(dist())
+    }
+  }
+}
+
+#' Generate arrivals stopping at a specified time
+#' 
+#' Generator convenience function to generate inter-arrivals with a specified stop time.
+#'
+#' @param stop_time the time at which to stop the generator.
+#' @param dist a function modelling the interarrival times.
+#'
+#' @return Returns a generator function.
+#' @seealso \link{add_generator}
+#' @export
+#'
+#' @examples
+#' t0 <- create_trajectory() %>% timeout(0)
+#' env <- simmer(verbose=T) %>%
+#'   add_generator("dummy", t0, to(5, function() runif(1, 1, 2))) %>%
+#'   run(10)
+to <- function(stop_time, dist) {
+  counter <- 0
+  function() {
+    dt <- dist()
+    counter <<- counter + dt
+    if (counter < stop_time) {
+      return(dt)
+    } else {
+      return(-1)
+    }
+  }
+}
+
+#' Generate arrivals starting and stopping at specified times
+#' 
+#' Generator convenience function to generate inter-arrivals with specified start and stop times.
+#'
+#' @param start_time the time at which to launch the initial arrival.
+#' @param stop_time the time at which to stop the generator.
+#' @param dist a function modelling the interarrival times.
+#' @param arrive if set to \code{TRUE} (default) the first arrival will be 
+#' generated at \code{start_time} and will follow \code{dist} from then on. 
+#' If set to \code{FALSE}, will initiate \code{dist} at \code{start_time} 
+#' (and the first arrival will most likely start at a time later than 
+#' \code{start_time}).
+#' 
+#' @return Returns a generator function.
+#' @seealso \link{add_generator}
+#' @export
+#'
+#' @examples
+#' t0 <- create_trajectory() %>% timeout(0)
+#' env <- simmer(verbose=T) %>%
+#'   add_generator("dummy", t0, from_to(5, 10, function() runif(1, 1, 2))) %>%
+#'   run(100)
+from_to <- function(start_time, stop_time, dist, arrive=TRUE){
+  started <- FALSE
+  counter <- 0
+  function() {
+    if (!started) {
+      started <<- TRUE
+      if (arrive) {
+        dt <- start_time
+      } else {
+        dt <- start_time + dist()
+      }
+    } else {
+      dt <- dist()
+    }
+    counter <<- counter + dt
+    if (counter < stop_time) {
+      return(dt)
+    } else {
+      return(-1)
     }
   }
 }
