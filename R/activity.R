394682c2ca0bc989445b305a32ec38f126433f1c
Iñaki Úcar <i.ucar86@gmail.com>
2016-07-03 21:42:08 +0200

    doc refactoring

--- R/activity.R
+++ R/activity.R
@@ -4,28 +4,22 @@
 #'
 #' @param activity an external pointer to the activity.
 #' 
-#' @seealso \code{\link{get_next_activity}}, \code{\link{get_prev_activity}}.
+#' @seealso \code{\link{get_head}}, \code{\link{get_tail}}, 
+#' \code{\link{get_next_activity}}, \code{\link{get_prev_activity}}.
 #' @export
 print_activity <- function(activity) activity_print_(activity, 0)
 
-#' Get the next activity
+#' Get the next/prev activity
 #' 
-#' It takes an external pointer to an activity an returns the next activity.
+#' It takes an external pointer to an activity an returns the next/prev activity.
 #'
 #' @param activity an external pointer to the activity.
 #' 
 #' @return An external pointer to an activity object.
-#' @seealso \code{\link{print_activity}}, \code{\link{get_prev_activity}}.
+#' @seealso \code{\link{get_head}}, \code{\link{get_tail}}, \code{\link{print_activity}}.
 #' @export
 get_next_activity <- function(activity) activity_get_next_(activity)
 
-#' Get the previous activity
-#' 
-#' It takes an external pointer to an activity an returns the previous activity.
-#'
-#' @param activity an external pointer to the activity.
-#' 
-#' @return An external pointer to an activity object.
-#' @seealso \code{\link{print_activity}}, \code{\link{get_next_activity}}.
+#' @rdname get_next_activity
 #' @export
 get_prev_activity <- function(activity) activity_get_prev_(activity)
--- R/trajectory.R
+++ R/trajectory.R
@@ -221,33 +221,26 @@ simmer.trajectory$public_methods$clone <- simmer.trajectory$private_methods$copy
 #' t1
 create_trajectory <- function(name="anonymous", verbose=FALSE) simmer.trajectory$new(name, verbose)
 
-#' Get the first activity
+#' Get the first/last activity
 #'
-#' It can be used to get the pointer to a trajectory's first activity.
+#' Gets the pointer to a trajectory's first/last activity.
 #' 
 #' @param traj the trajectory object.
 #' 
 #' @return An external pointer to an activity object.
-#' @seealso \code{\link{get_tail}}, \code{\link{get_n_activities}}, \code{\link{join}}.
+#' @seealso \code{\link{get_n_activities}}, \code{\link{join}}.
 #' @export
 get_head <- function(traj) traj$get_head()
 
-#' Get the last activity
-#'
-#' It can be used to get the pointer to a trajectory's last activity.
-#' 
-#' @param traj the trajectory object.
-#' 
-#' @return An external pointer to an activity object.
-#' @seealso \code{\link{get_head}}, \code{\link{get_n_activities}}, \code{\link{join}}.
+#' @rdname get_head
 #' @export
 get_tail <- function(traj) traj$get_tail()
 
 #' Get the number of activities
 #'
-#' It can be used to get the total number of activities defined inside a trajectory.
+#' Gets the total number of activities defined inside a trajectory.
 #' 
-#' @param traj the trajectory object.
+#' @inheritParams get_head
 #' 
 #' @return The number of activities in the trajectory.
 #' @seealso \code{\link{get_head}}, \code{\link{get_tail}}, \code{\link{join}}.
@@ -281,18 +274,18 @@ join <- function(...) {
   traj[[1]]
 }
 
-#' Add a seize activity
+#' Add a seize/release activity
 #'
-#' Adds a new activity capable of seizing a resource to the tail of a trajectory.
+#' Adds a new activity capable of seizing/releasing a resource to the tail of a trajectory.
 #' 
-#' @param traj the trajectory object.
+#' @inheritParams get_head
+#' @inheritParams select
 #' @param resource the name of the resource.
-#' @param amount the amount to seize, accepts either a callable object (a function) or a numeric value.
+#' @param amount the amount to seize/release, accepts either a callable object (a function) or a numeric value.
 #' @param ... unused arguments
 #' 
 #' @return The trajectory object.
-#' @seealso \code{\link{release}}, \code{\link{seize_selected}}, 
-#' \code{\link{release_selected}}, \code{\link{select}}.
+#' @seealso \code{\link{select}}.
 #' @export
 seize <- function(traj, resource, amount=1, ...) {
   args <- list(...)
@@ -302,18 +295,7 @@ seize <- function(traj, resource, amount=1, ...) {
   traj$seize(resource, amount)
 }
 
-#' Add a seize activity
-#'
-#' Adds a new activity capable of seizing a previously selected resource to the tail of a trajectory.
-#' 
-#' @param traj the trajectory object.
-#' @param amount the amount to seize, accepts either a callable object (a function) or a numeric value.
-#' @param id selection identifier for nested usage.
-#' @param ... unused arguments
-#' 
-#' @return The trajectory object.
-#' @seealso \code{\link{seize}}, \code{\link{release}}, 
-#' \code{\link{release_selected}}, \code{\link{select}}.
+#' @rdname seize
 #' @export
 seize_selected <- function(traj, amount=1, id=0, ...) {
   args <- list(...)
@@ -323,31 +305,11 @@ seize_selected <- function(traj, amount=1, id=0, ...) {
   traj$seize(NA, amount, id)
 }
 
-#' Add a release activity
-#'
-#' Adds a new activity capable of releasing a resource to the tail of a trajectory.
-#' 
-#' @param traj the trajectory object.
-#' @param resource the name of the resource.
-#' @param amount the amount to release, accepts either a callable object (a function) or a numeric value.
-#' 
-#' @return The trajectory object.
-#' @seealso \code{\link{seize}}, \code{\link{seize_selected}}, 
-#' \code{\link{release_selected}}, \code{\link{select}}.
+#' @rdname seize
 #' @export
 release <- function(traj, resource, amount=1) traj$release(resource, amount)
 
-#' Add a release activity
-#'
-#' Adds a new activity capable of releasing a previously selected resource to the tail of a trajectory.
-#' 
-#' @param traj the trajectory object.
-#' @param amount the amount to release, accepts either a callable object (a function) or a numeric value.
-#' @param id selection identifier for nested usage.
-#' 
-#' @return The trajectory object.
-#' @seealso \code{\link{seize}}, \code{\link{release}}, \code{\link{seize_selected}}, 
-#' \code{\link{select}}.
+#' @rdname seize
 #' @export
 release_selected <- function(traj, amount=1, id=0) traj$release(NA, amount, id)
 
@@ -355,7 +317,7 @@ release_selected <- function(traj, amount=1, id=0) traj$release(NA, amount, id)
 #'
 #' Selects a resource for a subsequent seize/release.
 #' 
-#' @param traj the trajectory object.
+#' @inheritParams get_head
 #' @param resources one or more resource names, or a callable object (a function) which
 #' must return a resource name to select.
 #' @param policy if \code{resources} is a vector of names, this parameter determines
@@ -364,8 +326,7 @@ release_selected <- function(traj, amount=1, id=0) traj$release(NA, amount, id)
 #' @param id selection identifier for nested usage.
 #' 
 #' @return The trajectory object.
-#' @seealso \code{\link{seize}}, \code{\link{release}}, \code{\link{seize_selected}}, 
-#' \code{\link{release_selected}}.
+#' @seealso \code{\link{seize_selected}}, \code{\link{release_selected}}.
 #' @export
 select <- function(traj, resources, policy=c("shortest-queue", "round-robin", 
                                              "first-available", "random"), id=0)
@@ -376,7 +337,7 @@ select <- function(traj, resources, policy=c("shortest-queue", "round-robin",
 #' Adds a new activity capable of executing any user-defined task and setting
 #' an associated delay to the tail of a trajectory.
 #' 
-#' @param traj the trajectory object.
+#' @inheritParams get_head
 #' @param task the timeout duration supplied by either passing a numeric value or a 
 #' callable object (a function) that returns a numeric value (negative values are 
 #' automatically coerced to positive).
@@ -389,7 +350,7 @@ timeout <- function(traj, task) traj$timeout(task)
 #'
 #' Adds or modifies a key/value attribute. The value must be numeric.
 #' 
-#' @param traj the trajectory object.
+#' @inheritParams get_head
 #' @param key the attribute key (is coerced to a string).
 #' @param value the value to set, accepts either a numeric or a callable object 
 #' (a function) which returns a numeric.
@@ -402,7 +363,7 @@ set_attribute <- function(traj, key, value) traj$set_attribute(key, value)
 #'
 #' Modifies the arrival's prioritization values.
 #' 
-#' @param traj the trajectory object.
+#' @inheritParams get_head
 #' @param values expects either a vector/list or a callable object (a function)
 #' returning a vector/list of three values \code{c(priority, preemptible, restart)}.
 #' A negative value leaves the corresponding parameter unchanged.
@@ -416,7 +377,7 @@ set_prioritization <- function(traj, values) traj$set_prioritization(values)
 #'
 #' Adds a new activity that defines n alternative paths to the tail of a trajectory.
 #' 
-#' @param traj the trajectory object.
+#' @inheritParams get_head
 #' @param option a callable object (a function) that must return an integer 
 #' between 1 and \code{n}; it will be used by the arrivals to select a path to follow.
 #' @param continue a vector of \code{n} booleans that indicate whether the arrival must 
@@ -438,7 +399,7 @@ branch <- function(traj, option, continue, ..., merge="_deprecated") {
 #'
 #' Adds a new activity that goes backwards in the trajectory.
 #' 
-#' @param traj the trajectory object.
+#' @inheritParams get_head
 #' @param amount the amount of activities (of the same or parent trajectories) to roll back.
 #' @param times the number of repetitions until an arrival may continue.
 #' @param check a callable object (a function) that must return a boolean. If
@@ -453,7 +414,7 @@ rollback <- function(traj, amount, times=1, check) traj$rollback(amount, times, 
 #'
 #' Adds a new activity that terminates the trajectory with some probability.
 #' 
-#' @param traj the trajectory object.
+#' @inheritParams get_head
 #' @param prob a probability or a function returning a probability.
 #' 
 #' @return The trajectory object.
