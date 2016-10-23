d75d7289edacb567b2c611d6a206c004bb291ea4
Iñaki Úcar <i.ucar86@gmail.com>
2016-10-23 23:15:31 +0200

    add tests

--- tests/testthat/test-trajectory-send-trap.R
+++ tests/testthat/test-trajectory-send-trap.R
@@ -53,3 +53,51 @@ test_that("a signal is received while in a timeout", {
   expect_equal(arr$end_time, c(4, 4, 4))
   expect_equal(arr$activity_time, c(4, 3, 2))
 })
+
+test_that("a signal is received while blocked inside a resource", {
+  t <- create_trajectory() %>%
+    send("signal", 3) %>%
+    trap("signal") %>%
+    seize("res", 1) %>%
+    wait() %>%
+    timeout(1) %>%
+    release("res", 1)
+
+  env <- simmer(verbose = TRUE) %>%
+    add_resource("res", 1) %>%
+    add_generator("dummy", t, at(0, 1, 2)) %>%
+    run()
+  arr_glb <- get_mon_arrivals(env)
+  arr_res <- get_mon_arrivals(env, per_resource = TRUE)
+
+  expect_equal(arr_glb$start_time, c(0, 1, 2))
+  expect_equal(arr_glb$end_time, c(4, 5, 6))
+  expect_equal(arr_glb$activity_time, c(1, 1, 1))
+  expect_equal(arr_res$start_time, c(0, 1, 2))
+  expect_equal(arr_res$end_time, c(4, 5, 6))
+  expect_equal(arr_res$activity_time, c(1, 1, 1))
+})
+
+test_that("a signal is received while in a timeout inside a resource", {
+  t <- create_trajectory() %>%
+    send("signal", 3) %>%
+    trap("signal") %>%
+    seize("res", 1) %>%
+    timeout(10) %>%
+    timeout(1) %>%
+    release("res", 1)
+
+  env <- simmer(verbose = TRUE) %>%
+    add_resource("res", 1) %>%
+    add_generator("dummy", t, at(0, 1, 2)) %>%
+    run()
+  arr_glb <- get_mon_arrivals(env)
+  arr_res <- get_mon_arrivals(env, per_resource = TRUE)
+
+  expect_equal(arr_glb$start_time, c(0, 1, 2))
+  expect_equal(arr_glb$end_time, c(4, 5, 6))
+  expect_equal(arr_glb$activity_time, c(4, 1, 1))
+  expect_equal(arr_res$start_time, c(0, 1, 2))
+  expect_equal(arr_res$end_time, c(4, 5, 6))
+  expect_equal(arr_res$activity_time, c(4, 1, 1))
+})
--- working_dir/__working_file.R
+++ working_dir/__working_file.R
@@ -4,33 +4,18 @@ t <- create_trajectory() %>%
   send("signal", 3) %>%
   trap("signal") %>%
   seize("res", 1) %>%
-  wait() %>%
-  timeout(1) %>%
-  release("res", 1)
-
-env <- simmer(verbose = TRUE) %>%
-  add_generator("dummy", t, at(0, 1, 2)) %>%
-  add_resource("res", 1) %>%
-  run()
-env %>% get_mon_arrivals()
-env %>% get_mon_arrivals(per_resource = TRUE)
-
-t <- create_trajectory() %>%
-  send("signal", 3) %>%
-  trap("signal") %>%
-  seize("res", 1) %>%
   timeout(10) %>%
   timeout(1) %>%
   release("res", 1)
 
 env <- simmer(verbose = TRUE) %>%
-  add_generator("dummy", t, at(0, 1, 2)) %>%
   add_resource("res", 1) %>%
+  add_generator("dummy", t, at(0, 1, 2)) %>%
   run()
-env %>% get_mon_arrivals()
-env %>% get_mon_arrivals(per_resource = TRUE)
+get_mon_arrivals(env)
+get_mon_arrivals(env, per_resource = TRUE)
 
-#############################################################
+quit()
 
 t <- create_trajectory() %>%
   send("signal", 3) %>%
