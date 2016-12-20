04c796a940e8e11f082d30a0e01e87b14030ca52
Iñaki Úcar <i.ucar86@gmail.com>
2016-12-20 20:34:19 +0100

    update NEWS

--- working_dir/__working_file.R
+++ working_dir/__working_file.R
@@ -1,20 +1,3 @@
 library(simmer)
 
-t <- trajectory(verbose = TRUE) %>%
-  seize("asdf") %>%
-  timeout(1) %>%
-  wait() %>%
-  timeout(2) %>%
-  release("asdf")
 
-t
-
-join(t)
-
-head(t, 2)
-tail(t, 2)
-t[c(5, 3, 1)]
-t[-c(5, 3, 1)]
-t["timeout"]
-t[["timeout"]]
-t[[3]]
