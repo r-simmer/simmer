ad3f426739b2f4275a42dda8cd86e965b5798cb1
Iñaki Úcar <i.ucar86@gmail.com>
2018-04-22 12:56:38 +0200

    add monitor_delim

--- R/monitor.R
+++ R/monitor.R
@@ -61,44 +61,57 @@ monitor_mem <- function()
 #' @param path directory where files will be created (must exist).
 #' @param keep whether to keep files on exit. By default, files are removed.
 #' @param sep separator character.
+#' @param ext file extension to use.
 #' @param reader function that will be used to read the files.
-#' @param ... further arguments for \code{reader}.
+#' @param args a list of further arguments for \code{reader}.
 #'
 #' @details For large simulations, or if the RAM footprint is an issue, you may
-#' consider monitoring to disk. To that end, \code{monitor_csv} stores the values
-#' in comma-separated (CSV) files. The usual \code{\link{get_mon}_*} methods
-#' retrieve data frames from such files using the \code{reader} provided. By
-#' default, \code{\link[utils]{read.csv}} is used, but you may consider using
-#' faster alternatives from other packages. It is also possible to \code{keep}
-#' the files in a custom directory to read and post-process them in a separate
+#' consider monitoring to disk. To that end, \code{monitor_delim} stores the values
+#' in flat delimited files. The usual \code{\link{get_mon}_*} methods retrieve
+#' data frames from such files using the \code{reader} provided. By default,
+#' \code{\link[utils]{read.delim}} is used, but you may consider using faster
+#' alternatives from other packages. It is also possible to \code{keep} the
+#' files in a custom directory to read and post-process them in a separate
 #' workflow.
 #'
 #' @rdname monitor
-#' @importFrom utils read.csv
+#' @importFrom utils read.delim
 #' @export
-monitor_csv <- function(path=tempdir(), keep=FALSE, sep=",", reader=read.csv, ...) {
-  check_args(path="string", reader="function", sep="string")
+monitor_delim <- function(path=tempdir(), keep=FALSE, sep=" ", ext=".txt",
+                          reader=read.delim, args=list(stringsAsFactors=FALSE))
+{
+  check_args(path="string", sep="string", ext="string", reader="function", args="list")
 
   path <- path.expand(path)
   if (!dir.exists(path))
-    stop("monitor_csv: directory '", path, "' does not exist", call.=FALSE)
+    stop(match.call()[[1]], ": directory '", path, "' does not exist", call.=FALSE)
 
   files <- c(
-    arrivals = tempfile("arrivals_", path, ".csv"),
-    releases = tempfile("releases_", path, ".csv"),
-    attributes = tempfile("attributes_", path, ".csv"),
-    resources = tempfile("resources_", path, ".csv")
+    arrivals = tempfile("arrivals_", path, ext),
+    releases = tempfile("releases_", path, ext),
+    attributes = tempfile("attributes_", path, ext),
+    resources = tempfile("resources_", path, ext)
   )
   file.create(files)
 
   Monitor$new(
-    "to disk (CSV)",
-    CsvMonitor__new(files[1], files[2], files[3], files[4], sep),
+    "to disk (delimited files)",
+    CsvMonitor__new(files[[1]], files[[2]], files[[3]], files[[4]], sep),
     function(xptr, per_resource)
-      do.call(reader, list(ifelse(!per_resource, files[1], files[2])), ...),
-    function(xptr) do.call(reader, list(files[3], ...)),
-    function(xptr) do.call(reader, list(files[4], ...)),
+      do.call(reader, c(ifelse(!per_resource, files[[1]], files[[2]]), args)),
+    function(xptr) do.call(reader, c(files[[3]], args)),
+    function(xptr) do.call(reader, c(files[[4]], args)),
     files,
     function() if(!keep) unlink(files)
   )
 }
+
+#' @details \code{monitor_csv} is a special case of \code{monitor_delim} with
+#' \code{sep=","} and \code{ext=".csv"}.
+#'
+#' @rdname monitor
+#' @importFrom utils read.csv
+#' @export
+monitor_csv <- function(path=tempdir(), keep=FALSE,
+                        reader=read.csv, args=list(stringsAsFactors=FALSE))
+  monitor_delim(path, keep, ",", ".csv", reader, args)
