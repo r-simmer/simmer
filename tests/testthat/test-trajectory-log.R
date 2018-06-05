534c2a8cb6940eb77ae81259401e6601aff721d4
Iñaki Úcar <i.ucar86@gmail.com>
2018-06-05 13:04:10 +0200

    add tests

--- tests/testthat/test-trajectory-log.R
+++ tests/testthat/test-trajectory-log.R
@@ -1,21 +1,37 @@
 context("log")
 
-test_that("a message is correctly printed (1)", {
+test_that("messages are correctly printed depending on the log_level", {
   t <- trajectory() %>%
-    log_("hello world!")
+    log_(function() "Message 0") %>%
+    log_("Message 1", level=1) %>%
+    log_("Message 2", level=2) %>%
+    log_("Message 3", level=Inf)
 
-  env <- simmer(verbose = TRUE) %>%
-    add_generator("entity", t, at(5))
+  expect_output(
+    simmer(verbose=TRUE) %>%
+      add_generator("dummy", t, at(5)) %>%
+      run(),
+    "5: dummy0: Message 0"
+  )
 
-  expect_output(run(env), "5: entity0: hello world!")
-})
+  expect_output(
+    simmer(verbose=TRUE, log_level=1) %>%
+      add_generator("dummy", t, at(5)) %>%
+      run(),
+    "5: dummy0: Message 0.*5: dummy0: Message 1"
+  )
 
-test_that("a message is correctly printed (2)", {
-  t <- trajectory() %>%
-    log_(function() "hello world!")
+  expect_output(
+    simmer(verbose=TRUE, log_level=5) %>%
+      add_generator("dummy", t, at(5)) %>%
+      run(),
+    "5: dummy0: Message 0.*5: dummy0: Message 1.*5: dummy0: Message 2"
+  )
 
-  env <- simmer(verbose = TRUE) %>%
-    add_generator("entity", t, at(5))
-
-  expect_output(run(env), "5: entity0: hello world!")
+  expect_output(
+    simmer(verbose=TRUE, log_level=Inf) %>%
+      add_generator("dummy", t, at(5)) %>%
+      run(),
+    "5: dummy0: Message 0.*5: dummy0: Message 1.*5: dummy0: Message 2.*5: dummy0: Message 3"
+  )
 })
